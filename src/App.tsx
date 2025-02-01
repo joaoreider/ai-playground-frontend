2749ecdf0174a075fb906b9c0474d4333cc8e2bf
Joao Paulo <joaopauloj405@gmail.com>
2025-02-01 20:23:54 -0300

    chore: add Tailwind CSS with shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <Button>Click me</Button>
     </>
   )
 }
