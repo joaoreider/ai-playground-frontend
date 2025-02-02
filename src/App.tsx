9f1552e42ea5965b0f082351e50eeac5c3b162c4
Joao Paulo <joaopauloj405@gmail.com>
2025-02-01 22:07:42 -0300

    feat: app Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Github } from 'lucide-react'
 import { Button } from './components/ui/button'
 
 function App() {
-
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <div>
+      <div className='px-6 py-3 flex items-center justify-between  border-b'>
+
+        <h1 className='text-xl font-bold'>upload.ai</h1>
+        <Button variant='outline'>
+          <Github className='w-4 h-4 mr-2' />
+          Github
+          </Button>
+
+      </div>
+    </div>
   )
 }
 
